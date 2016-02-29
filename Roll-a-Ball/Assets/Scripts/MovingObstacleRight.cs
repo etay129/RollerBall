c8fbc15deba8e2ac7f88154f8d2afb2ade6544c5
etay129 <evangeline129@hotmail.com>
2016-02-28 19:08:46 -0800

    two level play

---
+++ Roll-a-Ball/Assets/Scripts/MovingObstacleRight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingObstacleRight : MonoBehaviour {
+	public int speed;
+
+	private int dx;
+
+	// Use this for initialization
+	void Start () {
+		dx = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Rigidbody rb = this.GetComponent<Rigidbody> ();
+		rb.velocity = new Vector3(speed*dx, 0, 0);
+		}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Trigger")) {
+			this.dx = dx*-1;
+		}
+	}
+
+}
---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+	public Text nextLevelText;
+
+	private Rigidbody rb;
+	private int count;
+	private bool win;
+
+	private Scene level1;
+	private Scene level2;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		win = false;
+		winText.text = "";
+		nextLevelText.text = "";
+		DisplayText ();
+
+		level1 = SceneManager.GetSceneByName ("Level 1");
+		level2 = SceneManager.GetSceneByName ("Level 2");
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void Update() {
+		if (win) {
+			if (Input.GetKeyDown("space")) {
+				Debug.Log ("next level");
+				SceneManager.LoadScene ("Level 2");
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count++;
+			DisplayText ();
+			}
+		}
+
+
+	void DisplayText () {
+		countText.text = "Gold Collected: " + count.ToString ();
+		if (count >= 12) {
+			if (level1.isLoaded) {
+				winText.text = "You've collected all the gold!";
+				nextLevelText.text = "Press 'Space' for Next Level";
+				win = true;
+			}
+			if (level2.isLoaded) {
+				winText.text = "You've collected all the gold!";
+			}
+		}
+	}
+
+}
+
